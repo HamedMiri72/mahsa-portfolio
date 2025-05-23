2b2c99c3a232bae610e194ab19cee6a3fb871e58
HamedMiri72 <hamedmiri72@gmail.com>
2025-05-23 12:28:28 +0100

    updatinf stair effect

--- src/components/ui/StairEffect.jsx
+++ src/components/ui/StairEffect.jsx
@@ -12,8 +12,24 @@ const StairEffect = () => {
 
     <AnimatePresence mode='wait'>
 
-        <div key={pathname} className='h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex'>
+        <div key={pathname}>
+          <div className='h-screen w-screen fixed top-0 left-0 right-0 z-40 flex pointer-events-none'>
             <Stairs/>
+        </div>
+
+        <motion.div
+        className='h-screen w-screen fixed bg-primary top-0 pointer-events-none'
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 0 ,
+          transition: {
+            duration: 0.4,
+            ease: "easeInOut",
+            delay: 0.4
+          }
+        }}
+        
+        />
+
         </div>
 
     </AnimatePresence>
